a2f514feb04ab8cb11f067f59108ae66100b3ddc
Ernesto Rico Schmidt <ernesto@helsinki.at>
2024-06-10 17:39:26 -0400

    Add reverse_sql strings and instructions

--- program/migrations/0014_add_fulltext_indices.py
+++ program/migrations/0014_add_fulltext_indices.py
@@ -5,13 +5,22 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
+    # this works with mysql 5.6, prior to this the engine needs to be MyISAM
+    # changing the engine does not work with 5.5 and the program_show tabl
     dependencies = [
         ("program", "0013_show_and_note_images"),
     ]
 
     operations = [
-        migrations.RunSQL("ALTER TABLE program_host ADD FULLTEXT (name);"),
-        migrations.RunSQL("ALTER TABLE program_note ADD FULLTEXT (title);"),
-        migrations.RunSQL("ALTER TABLE program_show ADD FULLTEXT (name);"),
+        migrations.RunSQL(
+            sql="ALTER TABLE program_host ADD FULLTEXT (name);",
+            reverse_sql="DROP INDEX name ON program_host;"
+        ),
+        migrations.RunSQL(
+            sql="ALTER TABLE program_note ADD FULLTEXT (title);",
+            reverse_sql="DROP INDEX title ON program_note;"),
+        migrations.RunSQL(
+            sql="ALTER TABLE program_show ADD FULLTEXT (name);",
+            reverse_sql="DROP INDEX name ON program_show;"
+        ),
     ]
